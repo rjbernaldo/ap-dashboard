76e6dadf34441e1dcf95a7677c0da60ae6474c35
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-14 09:59:35 +0800

    Add jest setup and initial test

---
+++ src/components/Root.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Root from './Root';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Root />,
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
