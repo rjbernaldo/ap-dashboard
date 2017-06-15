46d83ddf14cc72928e2a1d99d20f19c334cc0228
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-15 20:09:42 +0800

    Add custom style

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 import GHero from 'grommet/components/Hero';
 import GTabs from 'grommet/components/Tabs';
 import GTab from 'grommet/components/Tab';
+import GBox from 'grommet/components/Box';
 import GParagraph from 'grommet/components/Paragraph';
 import GButton from 'grommet/components/Button';
+import GAccordion from 'grommet/components/Accordion';
+import GAccordionPanel from 'grommet/components/AccordionPanel';
 
 const Home = () => (
   <div>
@@ -15,7 +18,18 @@ const Home = () => (
     </GParagraph>
     <GTabs>
       <GTab title="Integration Roadmap">
-        <h3>Admin</h3>
+        <h3 style={{'marginBottom': '0px'}}>Admin</h3>
+        <GBox pad="small">
+          <GAccordion openMulti={true}>
+            <GAccordionPanel heading="Integration task title">
+              <GBox pad="small">
+                <GParagraph>
+                  Vivamus rutrum risus et tincidunt ullamcorper. Sed aliquet euismod sapien vitae convallis. Nulla egestas elit sapien, nec rhoncus sem cursus in. Nullam a dignissim tortor, vitae lobortis felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                </GParagraph>
+              </GBox>
+            </GAccordionPanel>
+          </GAccordion>
+        </GBox>
         <h3>Planning</h3>
         <h3>Design</h3>
         <h3>Development</h3>
