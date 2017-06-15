b5d1cbb8d50206e86963cd15e2c1a410edb05c64
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-15 10:38:00 +0800

    Add initial set of links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div>Header</div>
+  <div>
+    <div>Header</div>
+    <Link to='/'>Home</Link>
+    <Link to='/analytics'>Analytics</Link>
+  </div>
 );
 
 export default Header;
